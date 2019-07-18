c648f31b293c64349a04e385aaf4878cc86158d1
beccaelenzil <beccaelenzil@gmail.com>
2019-07-18 10:42:49 -0700

    completed lesson 14

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,19 @@
 
-BOOKS = [
-  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
-  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
-  { title: "Kindred", author: "Octavia E. Butler"}
-]
+#BOOKS = [
+#  { title: "Hidden Figures", author: "Margot Lee Shetterly"},
+#  { title: "Practical Object-Oriented Design in Ruby", author: "Sandi Metz"},
+#  { title: "Kindred", author: "Octavia E. Butler"}
+#]
 
 
 class BooksController < ApplicationController
     def index
-       @books = BOOKS
+       @books = Book.all
     end
 
     def show
         book_id = params[:id].to_i
-        @book = BOOKS[book_id]
+        @book = Book.find(book_id)
         if @book.nil?
             head :not_found
         end
